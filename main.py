509837651244399ceedda6a2badb7d3eee0260fa
Rafael Ventura <raf4ael@gmail.com>
2021-08-12 13:49:32 -0300

    adicionando dados e script

---
+++ main.py
@@ -0,0 +1,27 @@
+# pandas, openpyxl, twilio
+import pandas as pd
+from twilio.rest import Client
+
+account_sid = "AC34a9d71f86088efc5fb0d2cbdf99a8b3"
+auth_token = "bc96b6b7b26c9d88ef52ae69ae184232"
+client = Client(account_sid, auth_token)
+lista_meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho']
+
+for mes in lista_meses:
+    tabelas = pd.read_excel(f'{mes}.xlsx')
+
+    if (tabelas['Vendas'] > 55000).any():
+
+        vendedor = tabelas.loc[tabelas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabelas.loc[tabelas['Vendas'] > 55000, 'Vendas'].values[0]
+        try:
+            message = client.messages.create(
+                to="+5521964488268",
+                from_="+17607904333",
+                body=
+                f"No mes {mes}, E Vendedore {vendedor} bateu a meta com {vendas}R$ de vendas."
+            )
+            print(f'mensagem enviada no id{message.sid}')
+        except NameError:
+            print('Não foi possivel enviar a mensagem')
+            print(NameError)
